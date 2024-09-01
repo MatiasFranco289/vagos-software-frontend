afe55fb4e3f57c7f92654b5a9e279ef9782b8f23
deshens <deshens@debian.com>
2024-09-01 16:59:55 -0300

    fixed styles and some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   return (
-    <div className=" w-full flex justify-center items-center h-[500px]">
+    <div className=" w-full flex justify-center items-center min-h-screen">
       <h1 className="text-6xl font-semibold">Vagos Software</h1>
     </div>
   );
