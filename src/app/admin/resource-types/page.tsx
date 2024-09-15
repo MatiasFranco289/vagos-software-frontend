776a5cfef0d0bd3193bc247d2a2d161cbc23a931
Matias Franco <matias.franco289@gmail.com>
2024-09-15 01:58:23 -0300

    VGS-79 now you can create a new resource type

--- src/app/admin/resource-types/page.tsx
+++ src/app/admin/resource-types/page.tsx
@@ -4,20 +4,34 @@ import axiosInstance from "@/axios";
 import LoadingScreen from "@/components/LoadingScreen/LoadingScreen";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import {
+  DEFAULT_API_ERROR_CLIENT_MESSAGE,
   DEFAULT_API_ERROR_MESSAGE,
   GET_ALL_RESOURCE_TYPES_API_URL,
+  POST_RESOURCE_TYPE_API_URL,
   ROLENAME_ADMIN,
+  STATUS_CODE_CREATED,
 } from "@/constants";
-import { getEnvironmentVariable, normalizeDate } from "@/utils";
+import {
+  getEnvironmentVariable,
+  manageRequestErrors,
+  normalizeDate,
+} from "@/utils";
 import { useEffect, useState } from "react";
 import GlowingButton from "@/components/GlowingButton/GlowingButton";
-import { useRouter } from "next/navigation";
+import ResourceTypeForm from "@/components/ResourceTypeForm/ResourceTypeForm";
+import { createResourceTypeSchema } from "@/validations/CreateResourceTypeValidation";
+import { ApiResponse } from "@/interfaces";
+import axios from "axios";
 
 export default function ResourceTypesList() {
   const apiBaseUrl = getEnvironmentVariable("NEXT_PUBLIC_BACKEND_BASE_URL");
   const getResourceTypesUrl = `${apiBaseUrl}${GET_ALL_RESOURCE_TYPES_API_URL}`;
+  const postResourceTypesUrl = `${apiBaseUrl}${POST_RESOURCE_TYPE_API_URL}`;
   const [resourceTypes, setResourceTypes] = useState<Array<ApiResourceType>>();
-  const router = useRouter();
+  const [newTypeModalOpen, setNewTypeModalOpen] = useState(false);
+
+  const [editTypeModalOpen, setEditTypeModalOpen] = useState(false);
+  const [actualTypeIdToEdit, setactualTypeIdToEdit] = useState(0);
 
   useEffect(() => {
     getAllResourceTypes();
@@ -33,11 +47,78 @@ export default function ResourceTypesList() {
     }
   };
 
+  const createResourceType = (newResourceTypeName: string) => {
+    const errorsToShow = {
+      "name must be unique": "El tipo de recurso ya existe.",
+    };
+
+    return new Promise<ApiResponse<ApiResourceType | null> | string>(
+      async (resolve, reject) => {
+        try {
+          const response = await axiosInstance.post(postResourceTypesUrl, {
+            name: newResourceTypeName,
+          });
+
+          if (response.status === STATUS_CODE_CREATED) {
+            return resolve(response.data);
+          }
+
+          reject(response.data.data);
+        } catch (err) {
+          let errorMessage = DEFAULT_API_ERROR_CLIENT_MESSAGE;
+
+          if (axios.isAxiosError(err) && err.response) {
+            const response = err.response.data as ApiResponse<unknown>;
+            const managedMessage = manageRequestErrors(response, errorsToShow);
+
+            errorMessage = managedMessage || errorMessage;
+          }
+
+          console.error(
+            "The following error has occurred while trying to create a new resource type: "
+          );
+          console.error(err);
+
+          reject(errorMessage);
+        }
+      }
+    );
+  };
+
+  const handleNewTypeClicked = () => {
+    document.body.classList.add("no-scroll");
+    setNewTypeModalOpen(true);
+  };
+  // TODO: Add update here
+  /* const handleResourceTypePressed = (id: number) => {
+    setactualTypeIdToEdit(id);
+    setEditTypeModalOpen(true);
+  }; */
+
   return (
     <ProtectedRoute requiredRole={ROLENAME_ADMIN}>
-      <div className=" w-full min-h-screen flex justify-center">
+      <div className="w-full min-h-screen flex justify-center">
         {(resourceTypes && (
-          <div className="mt-8 sm:mt-32 w-5/6">
+          <div className="mt-8 sm:mt-32 mb-32 sm:mb-12 w-5/6">
+            {newTypeModalOpen && (
+              <ResourceTypeForm
+                setModalOpen={setNewTypeModalOpen}
+                validationSchema={createResourceTypeSchema}
+                onFormSendPromise={createResourceType}
+              />
+            )}
+
+            {editTypeModalOpen && (
+              <ResourceTypeForm
+                setModalOpen={setNewTypeModalOpen}
+                validationSchema={createResourceTypeSchema}
+                onFormSendPromise={createResourceType}
+                initialValues={{
+                  name: resourceTypes[1].name,
+                }}
+              />
+            )}
+
             <div className="flex flex-col sm:flex-row justify-between mb-8">
               <h1 className="text-lg sm:text-2xl font-light whitespace-nowrap">
                 {"Admin > Tipos de recurso"}
@@ -46,12 +127,12 @@ export default function ResourceTypesList() {
               <div className="w-1/6 min-w-[120px] mt-4 sm:mt-0">
                 <GlowingButton
                   text="Nuevo tipo"
-                  onClick={() => router.push("resource-types/create")}
+                  onClick={() => handleNewTypeClicked()}
                 />
               </div>
             </div>
 
-            <div className="overflow-scroll rounded-t-md rounded-md">
+            <div className="overflow-scroll rounded-t-md rounded-md pointer">
               <h2 className="bg-dark-300 p-3 w-full min-w-[600px]">
                 Todos los tipos de recursos
                 <span className="text-white/50">{` ${resourceTypes.length}`}</span>
@@ -74,10 +155,9 @@ export default function ResourceTypesList() {
                         <tr
                           key={`resource_type_${index}`}
                           className="hover:bg-dark-400 duration-150 cursor-pointer"
-                          onClick={() =>
-                            router.push(
-                              `resource-types/edit/${resourceType.id}`
-                            )
+                          onClick={
+                            () => {}
+                            /* handleResourceTypePressed(resourceType.name) */
                           }
                         >
                           <td className="p-4">{resourceType.id}</td>
